54f6acb66804ea35b37a87fd28b2c5e3d6db8e5e
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-01-02 19:04:18 +0530

    hangman-text

--- src/hangmanLetter.tsx
+++ src/hangmanLetter.tsx
@@ -1,1 +1,25 @@
-export function HangmanLetter(){}
+export function HangmanLetter(){
+    const word = "TEST"
+    const guessedLetters = ["T"]
+    return(
+        <div 
+        style={{
+            display:"flex",
+            gap:"0.25em",
+            fontSize:"6rem",
+            fontWeight:"bold",
+            textTransform:"uppercase",
+            fontFamily:"monospace",
+        }}>
+            {word.split("").map((letter, index)=>(
+                <span style={{borderBottom:".1em solid black"}} key={index}>
+                    <span style={{
+                        visibility: guessedLetters.includes(letter)?"visible":"hidden",
+                    }}>
+                        {letter}
+                    </span>
+                </span>
+            ))}
+        </div>
+    )
+}
