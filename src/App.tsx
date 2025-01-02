e0c722fc892e3d5955dc4e16f1894b0a70c31508
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-01-02 15:43:50 +0530

    setup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react'
 import './App.css'
+import words from "./wordlist.json"
+import { HangmanDrawing } from './hangmandrawing'
+import { HangmanLetter } from './hangmanLetter'
+import { Keyboard } from './keyboard'
+
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(()=>{
+    return words[Math.floor(Math.random()* words.length)]
+  })
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  console.log(wordToGuess)
   return (
-    <>
-      <h1>Hangman</h1>
-    </>
+    <div 
+    style = {{
+      maxWidth:"800px",
+      display:"flex",
+      flexDirection:"column",
+      gap:"2rem",
+      margin:"0 auto",
+      alignItems:"center",
+    }}>
+      <div style={{fontSize:"2rem", textAlign: "center", }}>
+        Lose Win
+      </div>
+      <HangmanDrawing/>
+      <HangmanLetter/>
+      <Keyboard/>
+      
+    </div>
   )
 }
 
